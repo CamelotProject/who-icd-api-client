fc9ff5b500325e03148b554b8f6d626d858750e3
Gwendolen Lynch <gwendolen.lynch@gmail.com>
2021-08-30 13:40:45 +0200

    ICD-11 client

---
+++ src/Api/Client/Icd11.php
@@ -0,0 +1,199 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Camelot\WhoIcd\Api\Client;
+
+use Camelot\WhoIcd\Api\SearchQuery;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+final class Icd11
+{
+    private HttpClientInterface $httpClient;
+    private Token $tokenClient;
+    private string $apiVersion;
+    private string $language;
+
+    public function __construct(HttpClientInterface $httpClient, Token $tokenClient, string $apiVersion = 'v2', string $language = 'en')
+    {
+        $this->httpClient = $httpClient;
+        $this->tokenClient = $tokenClient;
+        $this->apiVersion = $apiVersion;
+        $this->language = $language;
+    }
+
+    /**
+     * Returns basic information on the latest release of the ICD-11 Foundation together with the top level Foundation
+     * entities.
+     *
+     * @param null|string $releaseId This is an optional parameter and if ignored, the API will return values from the
+     *                               latest released version of the Foundation. If provided, the API will respond using
+     *                               that particular release. The values are like "2019-04".
+     */
+    public function getFoundations(?string $releaseId = null): array
+    {
+        $options = $releaseId ? ['query' => ['releaseId' => $releaseId]] : [];
+
+        return $this->getClient()->request('GET', '/icd/entity', $options)->toArray();
+    }
+
+    /**
+     * Provides you information on a specific ICD-11 foundation entity.
+     *
+     * @param int         $id        Numeric part at the end of the URI for an entity
+     * @param null|string $releaseId This is an optional parameter and if ignored, the API will return values from the
+     *                               latest released version of the Foundation. If provided, the API will respond using
+     *                               that particular release. The values are like "2019-04".
+     */
+    public function getFoundation(int $id, ?string $releaseId = null): array
+    {
+        $options = $releaseId ? ['query' => ['releaseId' => $releaseId]] : [];
+
+        return $this->getClient()->request('GET', "/icd/entity/{$id}?", $options)->toArray();
+    }
+
+    /** Search the foundation component of the ICD-11. */
+    public function searchFoundation(SearchQuery $searchQuery): array
+    {
+        $options = ['query' => $searchQuery->toArray()];
+
+        return $this->getClient()->request('GET', '/icd/entity/search', $options)->toArray();
+    }
+
+    /**
+     * Returns basic information on the linearization such as MMS together with the list of available releases.
+     *
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics
+     */
+    public function getLinearization(string $linearizationName): array
+    {
+        return $this->getClient()->request('GET', "{$linearizationName}")->toArray();
+    }
+
+    /**
+     * Returns basic information on the released linearization (such as MMS) together with the chapters in it.
+     *
+     * @param string $releaseId         The id for the release. This is generally formatted like this: 2019-04
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     */
+    public function getReleaseLinearization(string $releaseId, string $linearizationName): array
+    {
+        return $this->getClient()->request('GET', "{$releaseId}/{$linearizationName}")->toArray();
+    }
+
+    /**
+     * Returns lists the URIs of the entity in the available releases.
+     *
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     * @param string $id                Numeric part at the end of the URI for an entity
+     */
+    public function getLinearizationById(string $linearizationName, string $id): array
+    {
+        return $this->getClient()->request('GET', "{$linearizationName}/{$id}")->toArray();
+    }
+
+    /**
+     * Returns lists the URIs of the residual entity in the available releases.
+     *
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     * @param string $id                Numeric part at the end of the URI for an entity. For residual categories this
+     *                                  number is the same as its parent but the residual categories also have other or
+     *                                  unspecified at the end.
+     * @param string $residual          For residual categories:
+     *                                  - this could be 'other' for other specified residual category
+     *                                  - or 'unspecified' for the unspecified residual category
+     */
+    public function getLinearizationByIdResidual(string $linearizationName, string $id, string $residual): array
+    {
+        return $this->getClient()->request('GET', "{$linearizationName}/{$id}/{$residual}")->toArray();
+    }
+
+    /**
+     * Return information on a linearization entity.
+     *
+     * @param string $releaseId         The id for the release. This is generally formatted like this: 2019-04
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     * @param string $id                Numeric part at the end of the URI for an entity
+     */
+    public function getReleaseLinearizationById(string $releaseId, string $linearizationName, string $id): array
+    {
+        return $this->getClient()->request('GET', "{$releaseId}/{$linearizationName}/{$id}")->toArray();
+    }
+
+    /**
+     * Return information on a residual linearization entity.
+     *
+     * @param string $releaseId         The id for the release. This is generally formatted like this: 2019-04
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     * @param string $id                Numeric part at the end of the URI for an entity
+     * @param string $residual          For residual categories:
+     *                                  - this could be 'other' for other specified residual category
+     *                                  - or 'unspecified' for the unspecified residual category
+     */
+    public function getReleaseLinearizationByIdResidual(string $releaseId, string $linearizationName, string $id, string $residual): array
+    {
+        return $this->getClient()->request('GET', "{$releaseId}/{$linearizationName}/{$id}/{$residual}")->toArray();
+    }
+
+    /**
+     * Look up an entity from its code. For postcoordinated code combinations, it provides more information on the code
+     * combination such as which axes are used and what values do they have.
+     *
+     * @param string $releaseId         The id for the release. This is generally formatted like this: 2019-04
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     * @param string $code              The code or code combination that will be looked up. (The & and / characters need to be URL encoded)
+     */
+    public function getReleaseLinearizationByCode(string $releaseId, string $linearizationName, string $code): array
+    {
+        return $this->getClient()->request('GET', "{$releaseId}/{$linearizationName}/codeinfo/{$code}")->toArray();
+    }
+
+    /**
+     * If the foundation entity is included in the linearization and has a code then that linearization entity is
+     * returned. If the foundation entity in included in the linearization but it is a grouping without a code then the
+     * system will return the unspecified residual category under that grouping.
+     *
+     * If the entity is not included in the linearization then the system checks where that entity is aggregated to and
+     * then returns that entity.
+     *
+     * @param string $releaseId         The id for the release. This is generally formatted like this: 2019-04
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     * @param string $foundationUri     The uri of the foundation entity that you'd like to map to this linearization
+     */
+    public function getReleaseLinearizationLookup(string $releaseId, string $linearizationName = null, string $foundationUri = null): array
+    {
+        $q = $linearizationName ? '?foundationUri=' . urlencode($linearizationName) : '';
+
+        return $this->getClient()->request('GET', "{$releaseId}/{$linearizationName}/lookup{$q}")->toArray();
+    }
+
+    /**
+     * Search the linearization (such as MMS). The search can be customized using the parameters as described Search endpoint.
+     *
+     * @param string $releaseId         The id for the release. This is generally formatted like this: 2019-04
+     * @param string $linearizationName Short name for the linearization. e.g. mms for ICD Mortality and Morbidity Statistics.
+     */
+    public function searchReleaseLinearization(string $releaseId, string $linearizationName, SearchQuery $searchQuery): array
+    {
+        $options = ['query' => $searchQuery->toArray()];
+
+        return $this->getClient()->request('GET', "{$releaseId}/{$linearizationName}/search", $options)->toArray();
+    }
+
+    private function getClient(): HttpClientInterface
+    {
+        return $this->httpClient->withOptions([
+            'base_uri' => 'https://id.who.int/icd/release/11/',
+            'headers' => [
+                'API-Version' => $this->apiVersion,
+                'Accept-Language' => $this->language,
+                'Authorization' => "Bearer {$this->getToken()}",
+            ],
+        ]);
+    }
+
+    private function getToken(): string
+    {
+        return $this->tokenClient->getToken();
+    }
+}
---
+++ tests/Api/Client/Icd11Test.php
@@ -0,0 +1,185 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Camelot\WhoIcd\Tests\Api\Client;
+
+use Camelot\WhoIcd\Api\Client\Icd11;
+use Camelot\WhoIcd\Api\Client\Token;
+use Camelot\WhoIcd\Api\SearchQuery;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\HttpClient;
+
+/**
+ * @covers \Camelot\WhoIcd\Api\Client\Icd11
+ *
+ * @internal
+ */
+final class Icd11Test extends TestCase
+{
+    public function providerFoundations(): iterable
+    {
+        yield 'Null' => ['2021-05', null];
+        yield 'Empty string' => ['2021-05', ''];
+        yield '2019-04' => ['2019-04', '2019-04'];
+        yield '2021-05' => ['2021-05', '2021-05'];
+    }
+
+    /** @dataProvider providerFoundations */
+    public function testGetFoundations(string $expected, ?string $releaseId): void
+    {
+        $result = $this->getClient()->getFoundations($releaseId);
+
+        static::assertSame($expected, $result['releaseId']);
+    }
+
+    public function providerFoundation(): iterable
+    {
+        yield [
+            'Autism spectrum disorder without disorder of intellectual development and with mild or no impairment of functional language',
+            120443468,
+            null,
+        ];
+        yield [
+            'Autism spectrum disorder without disorder of intellectual development and with mild or no impairment of functional language',
+            120443468,
+            '2019-04',
+        ];
+    }
+
+    /** @dataProvider providerFoundation */
+    public function testGetFoundation(string $expected, int $id, ?string $releaseId): void
+    {
+        $result = $this->getClient()->getFoundation($id, $releaseId);
+
+        static::assertSame($expected, $result['title']['@value']);
+    }
+
+    public function providerSearchFoundation(): iterable
+    {
+        yield ['Attention deficit hyperactivity disorder', SearchQuery::create('adhd')->setHighlightingEnabled(false)];
+    }
+
+    /** @dataProvider providerSearchFoundation */
+    public function testSearchFoundation(string $expected, SearchQuery $query): void
+    {
+        $result = $this->getClient()->searchFoundation($query);
+
+        static::assertNotCount(0, $result['destinationEntities']);
+        $this->addToAssertionCount(1);
+        foreach ($result['destinationEntities'] as $entity) {
+            if ($entity['title'] === $expected) {
+                return;
+            }
+        }
+        static::fail("Did not find expected entity with title '{$expected}'");
+    }
+
+    public function providerLinearization(): iterable
+    {
+        yield ['International Classification of Diseases 11th Revision - ICD-11 for Mortality and Morbidity Statistics', 'mms'];
+    }
+
+    /** @dataProvider providerLinearization */
+    public function testGetLinearization(string $expected, string $linearization): void
+    {
+        $result = $this->getClient()->getLinearization($linearization);
+
+        static::assertSame($expected, $result['title']['@value']);
+    }
+
+    public function providerReleaseLinearization(): iterable
+    {
+        yield ['International Classification of Diseases 11th Revision - Mortality and Morbidity Statistics', '2019-04', 'mms'];
+    }
+
+    /** @dataProvider providerReleaseLinearization */
+    public function testGetReleaseLinearization(string $expected, string $releaseId, string $linearizationName): void
+    {
+        $result = $this->getClient()->getReleaseLinearization($releaseId, $linearizationName);
+
+        static::assertSame($expected, $result['title']['@value']);
+        static::assertSame($releaseId, $result['releaseId']);
+    }
+
+    public function testGetLinearizationById(): void
+    {
+        $result = $this->getClient()->getLinearizationById('mms', '21500692');
+
+        static::assertSame('Endocrine, nutritional or metabolic diseases', $result['title']['@value']);
+    }
+
+    public function testGetReleaseLinearizationByIdResidual(): void
+    {
+        static::markTestIncomplete();
+
+        $result = $this->getClient()->getReleaseLinearizationByIdResidual('2019-04', 'mms', '21500692', 'other');
+        $expected = '';
+
+        static::assertSame($expected, $result);
+    }
+
+    public function testGetReleaseLinearizationById(): void
+    {
+        $result = $this->getClient()->getReleaseLinearizationById('2021-05', 'mms', '21500692');
+        $expected = 'Endocrine, nutritional or metabolic diseases';
+
+        static::assertSame($expected, $result['title']['@value']);
+    }
+
+    public function testGetLinearizationByIdResidual(): void
+    {
+        static::markTestIncomplete();
+
+        $result = $this->getClient()->getLinearizationByIdResidual('mms', '21500692', 'other');
+        $expected = '';
+
+        static::assertSame($expected, $result);
+    }
+
+    public function testGetReleaseLinearizationByCode(): void
+    {
+        $result = $this->getClient()->getReleaseLinearizationByCode('2021-05', 'mms', '02');
+        $expected = 'http://id.who.int/icd/release/11/2021-05/mms/codeinfo/02';
+
+        static::assertSame($expected, $result['@id']);
+    }
+
+    public function testGetReleaseLinearizationLookup(): void
+    {
+        static::markTestIncomplete();
+
+        $result = $this->getClient()->getReleaseLinearizationLookup('2021-05');
+        $expected = '';
+
+        static::assertSame($expected, $result);
+    }
+
+    public function testSearchReleaseLinearization(): void
+    {
+        $result = $this->getClient()->searchReleaseLinearization('2019-04', 'mms', SearchQuery::create('autism%')->setHighlightingEnabled(false));
+        $expected = 'Autism spectrum disorder, unspecified';
+
+        static::assertNotCount(0, $result['destinationEntities']);
+
+        $this->addToAssertionCount(1);
+        foreach ($result['destinationEntities'] as $entity) {
+            if ($entity['title'] === $expected) {
+                return;
+            }
+        }
+        static::fail("Did not find destinationEntities value with title: '{$expected}'");
+    }
+
+    private function getClient(): Icd11
+    {
+        return new Icd11(HttpClient::create(), $this->getTokenClient(), 'v2', 'en');
+    }
+
+    private function getTokenClient(): Token
+    {
+        $client = HttpClient::create();
+
+        return new Token($client, $_ENV['WHO_CLIENT_ID'], $_ENV['WHO_CLIENT_SECRET']);
+    }
+}
